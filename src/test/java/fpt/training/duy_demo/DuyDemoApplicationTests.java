4f3017989cc4861f240a1c7b73782f40a1c18704
Duy <hoangnguyen4869@gmail.com>
2024-07-23 20:32:42 +0700

    init project

---
+++ src/test/java/fpt/training/duy_demo/DuyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.training.duy_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
